03afdc6ecf73a5d1e4050af573eb7704c87945f9
YairHernandezPech <edilsonchelitohernandez011@gmail.com>
2026-07-01 21:24:04 -0600

    Eliminar node_modules del repositorio

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const verificarToken = require("../middlewares/auth");
 
+const userController = require("../controller/usuario.controller");
+
+//USUARIOS
+router.post("/register", userController.registerUser);
+router.post("/login", userController.loginUser);
+router.get("/usuarios", verificarToken, userController.getUsers);
 
 module.exports = router;
